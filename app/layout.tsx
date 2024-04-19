b7355e4112b3592c426d84b45d15c1a8d92c16d3
lgcruz <luisginocruz_95@hotmail.com>
2024-04-19 12:32:15 -0400

    next commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,15 @@
+import '@/app/ui/global.css';
+import { inter, lusitana } from '@/app/ui/fonts';
+
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className={`${inter.variable} ${lusitana.variable}`}>
+      <body className={`font-lusitana antialiased`}>{children}</body>
     </html>
   );
 }
